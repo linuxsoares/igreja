d3b295070f0a42ffe280761139a730d05906c717
linuxsoares <linux.soares@gmail.com>
2013-08-24 11:08:50 -0700

    Alteração URL para funcionar STATIC

--- igreja_cadastro/urls.py
+++ igreja_cadastro/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,4 +16,10 @@ urlpatterns = patterns('igreja_cadastro.core.views',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT
+    }),
 )
