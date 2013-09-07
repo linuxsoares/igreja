174a81ae764f1497551c9a718792254794e0230b
linuxsoares <linux.soares@gmail.com>
2013-09-07 12:41:24 -0700

    Validação do campo CPF

--- igreja_cadastro/cadastro_fieis/admin.py
+++ igreja_cadastro/cadastro_fieis/admin.py
@@ -1,6 +1,20 @@
 # coding: utf-8
 
 from django.contrib import admin
+from django.utils.datetime_safe import datetime
+from django.utils.translation import ugettext as _
 from igreja_cadastro.cadastro_fieis.models import CadastroFieis
 
-admin.site.register(CadastroFieis)
+class CadastroFieisAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cpf', 'criado_em', 'cadastrado_hoje')
+    date_hierarchy = 'criado_em'
+    search_fields = ('nome', 'email', 'cpf', 'criado_em')
+    list_filter = ['criado_em']
+
+    def cadastrado_hoje(self, obj):
+        return obj.criado_em.date() == datetime.today().date()
+
+    cadastrado_hoje.short_description = _(u'Cadastrado Hoje?')
+    cadastrado_hoje.boolean = True
+
+admin.site.register(CadastroFieis, CadastroFieisAdmin)
--- igreja_cadastro/cadastro_fieis/forms.py
+++ igreja_cadastro/cadastro_fieis/forms.py
@@ -2,7 +2,12 @@
 
 from django import forms
 from django.utils.translation import ugettext as _
+from django.core.exceptions import ValidationError
 from igreja_cadastro.cadastro_fieis.models import CadastroFieis
+
+def CPFValid(value):
+    if not value.isdigit():
+        raise ValidationError(_(u'CPF Deve conter apenas Números!'))
 
 SEXO_CHOICES = (
     (0, 'Masculino'),(1, 'Feminino')
@@ -15,3 +20,8 @@ ESTADO_CIVIL = (
 class CadastroForm(forms.ModelForm):
     class Meta:
         model = CadastroFieis
+
+    def __init__(self, *args, **kwargs):
+        super(CadastroForm, self).__init__(*args, **kwargs)
+
+        self.fields['cpf'].validators.append(CPFValid)
