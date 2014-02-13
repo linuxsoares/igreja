cf720b8755056ccb2eb44e02cbd8e85705a7c53c
linuxsoares <linux.soares@gmail.com>
2014-02-12 22:23:51 -0200

    Upload FOTO

--- igreja_cadastro/cadastro_fieis/views.py
+++ igreja_cadastro/cadastro_fieis/views.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from xml.dom.minidom import Document
 
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
@@ -8,7 +9,7 @@ from igreja_cadastro.cadastro_fieis.models import CadastroFieis
 
 def cadastro(request):
     if request.method == 'POST':
-        form = CadastroForm(request.POST)
+        form = CadastroForm(request.POST, request.FILES)
         if form.is_valid():
             obj = form.save()
             return HttpResponseRedirect('/cadastro/%d/' % obj.pk)
