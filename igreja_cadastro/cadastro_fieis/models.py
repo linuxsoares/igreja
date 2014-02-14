27f48f0059ebac7035fd1622d4aa01d56cbbeccb
linuxsoares <linux.soares@gmail.com>
2014-02-13 22:56:49 -0200

    Static Files

--- igreja_cadastro/cadastro_fieis/models.py
+++ igreja_cadastro/cadastro_fieis/models.py
@@ -67,7 +67,7 @@ class CadastroFieis(models.Model):
     foto = models.FileField(_('foto'), upload_to='images/', null=True, blank=True)
 
     def image_tag(self):
-        return u'<img src="%s" />' % self.foto
+        return u'<img src="%s" width="180px" height="180px"/>' % self.foto
     image_tag.short_description = 'Imagem'
     image_tag.allow_tags = True
 
